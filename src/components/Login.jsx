542aba9c2010c7ff7a34075ad23062c4821e5874
AYUSH GUPTA <116937826+ayush-gupta456@users.noreply.github.com>
2025-07-27 18:42:05 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { CopyIcon, DeleteIcon, EditIcon, EyeIcon, EyeSlashIcon, GenerateIcon, SaveIcon } from './Icons';
 
 /**
  * The Login component handles user authentication.
